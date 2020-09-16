4cf629e5f844ebe42ae588317d16a6ed5b1dca66
zt <zt@wizkids.dk>
2020-09-16 16:05:56 +0200

    add script to gmail message page

---
+++ gmail-message/script.js
@@ -0,0 +1,20 @@
+/** @type {!HTMLDivElement} */
+const div = document.querySelector('div.editable');
+
+div.focus();
+
+const range = new Range();
+range.selectNode(div);
+
+const textElement = div.firstElementChild.childNodes[0];
+
+range.setStart(textElement, 3);
+range.setEnd(textElement, 3);
+
+const sel = document.getSelection();
+sel.removeAllRanges();
+sel.addRange(range);
+
+['keydown', 'keypress', 'keyup'].forEach(x => {
+  div.dispatchEvent(new KeyboardEvent(x, { bubbles: true, cancelable: false }));
+});
