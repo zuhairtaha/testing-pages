ad5bb5595c11660ab83fc6c0c24575e0975a1615
Zuhair Taha <zt@wizkids.dk>
2021-11-11 09:52:54 +0100

    Add Input field contains clipped text

---
+++ input-highlight/app.js
@@ -0,0 +1,4 @@
+document.querySelectorAll('input[type=text]').forEach(input => {
+  input.value =
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est minima voluptatum delectus aut officia modi dolorum incidunt, quis cum repellat praesentium natus sit blanditiis labore eos, similique, earum asperiores impedit?';
+});
