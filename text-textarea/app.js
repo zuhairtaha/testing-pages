10d3318a5ce8b7c597959c8dbed0ec3ed6a7851c
Zuhair Taha <zt@wizkids.dk>
2022-01-27 16:08:47 +0100

    Update test toolbar position

--- text-textarea/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const selection = window.getSelection();
-  const range = new Range();
-  range.selectNodeContents(document.querySelector('main'));
-  selection.removeAllRanges();
-  selection.addRange(range);
-});
