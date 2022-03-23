19645b3c8451ed0b6ade96cc1b04035a86d9a724
Zuhair Taha <zt@wizkids.dk>
2022-03-23 14:14:24 +0100

    Update

---
+++ nested-shadow-dom/CustomElements.js
@@ -0,0 +1,36 @@
+class OuterElement extends HTMLElement {
+  constructor() {
+    super();
+    /** @type {!ShadowRoot} */
+    this._root = this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this._render();
+  }
+
+  _render() {
+    this._root.innerHTML = `
+  <style>
+    :host {
+      display: block;
+      background: rgba(255, 0, 0, 0.1);
+      font-size: 24px;
+      border: solid 1px rgba(0, 0, 0, 0.5);
+      width: 50%;
+      padding: 20px;
+    }
+  </style>
+  <span>${this.tagName.toLocaleLowerCase()}</span>
+  <slot></slot>
+  `;
+  }
+}
+
+customElements.define('outer-element', OuterElement);
+
+class InnerElement extends OuterElement {}
+customElements.define('inner-element', InnerElement);
+
+class depperElement extends OuterElement {}
+customElements.define('deeper-element', depperElement);
