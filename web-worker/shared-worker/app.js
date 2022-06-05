acb091403c704c8720cac2fda2c08fbeb8618fbe
Zuhair Taha <zt@wizkids.dk>
2022-06-05 15:04:14 +0200

    Update

--- web-worker/shared-worker/app.js
+++ web-worker/shared-worker/app.js
@@ -1,3 +1,6 @@
+// Cross-Tab Communication: Open the same tab or any other tab includes app.js 
+// Then you can get the messsage in all of the tabs.
+
 const worker = new SharedWorker('shared-worker.js', { name: 'counter-worker' });
 worker.port.onmessage = function (event) {
   console.log(event.data);
