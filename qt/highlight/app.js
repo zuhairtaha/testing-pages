26ac16e905239a19932397a20d5b2fb96c5a663d
Zuhair Taha <zuta@queue-it.com>
2026-02-23 12:06:48 +0100

    feat: add highlight options and update highlight colors

--- qt/highlight/app.js
+++ qt/highlight/app.js
@@ -1,8 +1,7 @@
-const button = /** @type {!HTMLButtonElement} */ (document.querySelector("button"));
-const p = /** @type {!HTMLParagraphElement} */ (document.querySelector("p"));
-const node = p.childNodes[0];
+const node = document.querySelector("p")?.childNodes[0];
+if (!node) throw new Error("No text node found");
 
-button.addEventListener("click", evt => {
+document.querySelector("#set-sample")?.addEventListener("click", evt => {
   evt.stopPropagation();
 
   const rangeA = new Range();
@@ -13,7 +12,7 @@ button.addEventListener("click", evt => {
   rangeB.setStart(node, 97);
   rangeB.setEnd(node, 102);
 
-  CSS.highlights.set("search-term-highlight", new Highlight(rangeA, rangeB));
+  CSS.highlights.set("yellow-highlight", new Highlight(rangeA, rangeB));
 
   const rangeC = new Range();
   rangeC.setStart(node, 49);
@@ -21,6 +20,17 @@ button.addEventListener("click", evt => {
   CSS.highlights.set("blue-highlight", new Highlight(rangeC));
 });
 
-document.addEventListener("click", () => {
+document.querySelector("#clear-sample")?.addEventListener("click", () => {
   CSS.highlights.clear();
+});
+
+document.querySelector("#set-on-selection")?.addEventListener("click", () => {
+  const selection = window.getSelection();
+  if (selection && selection.rangeCount > 0) {
+    const range = selection.getRangeAt(0);
+    const highlightType = /** @type {HTMLSelectElement} */ (document.querySelector("#highlight-options")).value;
+    CSS.highlights.set(highlightType, new Highlight(range));
+  } else {
+    alert("Please select some text to highlight.");
+  }
 });
